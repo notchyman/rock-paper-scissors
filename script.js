dd4a3c7af1ea870b31b87a039a94c89c99b05130
notchyman <77229151+notchyman@users.noreply.github.com>
2023-08-02 13:34:33 -0700

    added 3 buttons for player to choose rock, paper or scissors.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+let playerSelection = ""; 
+
 let playerScore = 0;
 let computerScore = 0;
 
@@ -6,47 +9,44 @@ function getComputerChoice() {
     let selection = arr[Math.floor(Math.random() * arr.length)];
     return selection;
 }
-
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "paper") {
-        computerScore += 1;
         return "computer";
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        computerScore += 1;
         return "computer";
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        computerScore += 1;
         return "computer";
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        playerScore += 1;
         return "player";
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        playerScore += 1;
         return "player";
     } else if (playerSelection === "paper" && computerSelection === "rock") {
-        playerScore += 1;
         return "player";
     } else {
         return "tie";
     }
 }
 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        let playerSelection = prompt("Rock, paper, or scissors?").toLowerCase();
-        let computerSelection = getComputerChoice();
-        let result = playRound(playerSelection, computerSelection);
+function game(playerSelection) {
+    let computerSelection = getComputerChoice();
+    let result = playRound(playerSelection, computerSelection);
 
-        if (result === "player") {
-            console.log("You win this round!");
-        } else if (result === "computer") {
-            console.log("Computer wins this round!");
-        } else {
-            console.log("It's a tie!");
-        }
-
-        console.log(`Player Score: ${playerScore}, Computer Score: ${computerScore}`);
+    if (result === "player") {
+        console.log("You win this round!");
+        playerScore += 1;
+    } else if (result === "computer") {
+        console.log("Computer wins this round!");
+        computerScore += 1;
+    } else {
+        console.log("It's a tie!");
     }
+
+    console.log(`Player Score: ${playerScore}, Computer Score: ${computerScore}`);
 }
 
-game();
+const buttons = document.querySelectorAll("#button1, #button2, #button3");
+buttons.forEach(function(button) {
+    button.addEventListener("click", function() {
+        game(button.textContent.toLowerCase());
+    });
+});
