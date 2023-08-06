45946ca525c9e6e814b371fd85dd675908a084c0
notchyman <77229151+notchyman@users.noreply.github.com>
2023-08-05 22:15:11 -0700

    added a background image, background gradient, and split the page into 3 columns

--- script.js
+++ script.js
@@ -1,75 +1,84 @@
-const body = document.body
-let div = document.createElement("div")
+// const body = document.body;
+const body = document.getElementById("gameBox")
+let div = document.createElement("div");
 
-var list = document.getElementById('demo');
+var list = document.getElementById("list");
 
-let playerSelection = ""; 
+let playerSelection = "";
 
 let playerScore = 0;
 let computerScore = 0;
 let roundCount = 0;
 
 function getComputerChoice() {
-    let arr = ["rock", "paper", "scissors"];
-    let selection = arr[Math.floor(Math.random() * arr.length)];
-    return selection;
+  let arr = ["rock", "paper", "scissors"];
+  let selection = arr[Math.floor(Math.random() * arr.length)];
+  return selection;
 }
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === "rock" && computerSelection === "paper") {
-        return "computer";
-    } else if (playerSelection === "paper" && computerSelection === "scissors") {
-        return "computer";
-    } else if (playerSelection === "scissors" && computerSelection === "rock") {
-        return "computer";
-    } else if (playerSelection === "rock" && computerSelection === "scissors") {
-        return "player";
-    } else if (playerSelection === "scissors" && computerSelection === "paper") {
-        return "player";
-    } else if (playerSelection === "paper" && computerSelection === "rock") {
-        return "player";
-    } else {
-        return "tie";
-    }
+  if (playerSelection === "rock" && computerSelection === "paper") {
+    return "computer";
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return "computer";
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return "computer";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return "player";
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return "player";
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return "player";
+  } else {
+    return "tie";
+  }
 }
 
 function game(playerSelection) {
-    let computerSelection = getComputerChoice();
-    let result = playRound(playerSelection, computerSelection);
+  let computerSelection = getComputerChoice();
+  let result = playRound(playerSelection, computerSelection);
 
-    let resultMessage = document.createElement("li")
+  let resultMessage = document.createElement("li");
 
-    if (roundCount < 5)
-        if (result === "player") {
-            playerScore += 1;
-            roundCount += 1;
-            resultMessage.textContent = "You win this round!";
-            list.append(resultMessage);
-        } else if (result === "computer") {
-            computerScore += 1;
-            roundCount += 1;
-            resultMessage.textContent = "Computer wins this round!";
-            list.append(resultMessage);
-        } else {
-            roundCount += 1;
-            resultMessage.textContent = "It's a tie!";
-            list.append(resultMessage);
-        }
-        /* console.log(`Player Score: ${playerScore}, Computer Score: ${computerScore}`); */
+  document.getElementById("displayPlayerScore").innerHTML = playerScore;
+  document.getElementById("displayComputerScore").innerHTML = computerScore;
 
-        if (roundCount == 5 && playerScore > computerScore) {
-            div.innerText = "Player wins the match!"
-            body.append(div)
-        } else if (roundCount == 5 && computerScore > playerScore) {
-            div.innerText = "Computer wins the match!"
-            div.append
-            body.append(div)
-        }
 
+
+  if (roundCount < 5)
+    if (result === "player") {
+      playerScore += 1;
+      roundCount += 1;
+      resultMessage.textContent = "You win this round!";
+      list.append(resultMessage);
+      document.getElementById("displayPlayerScore").innerHTML = playerScore;
+    } else if (result === "computer") {
+      computerScore += 1;
+      roundCount += 1;
+      resultMessage.textContent = "Computer wins this round!";
+      document.getElementById("displayComputerScore").innerHTML = computerScore;
+      list.append(resultMessage);
+    } else {
+      roundCount += 1;
+      resultMessage.textContent = "It's a tie!";
+      list.append(resultMessage);
+    }
+  /* console.log(`Player Score: ${playerScore}, Computer Score: ${computerScore}`); */
+
+  if (roundCount == 5 && playerScore > computerScore) {
+    div.innerText = "Player wins the match!";
+    body.append(div);
+  } else if (roundCount == 5 && computerScore > playerScore) {
+    div.innerText = "Computer wins the match!";
+    body.append(div);
+  } else if (roundCount == 5 && computerScore == playerScore) {
+    div.innerText = "The match is a tie!";
+    body.append(div);
+  }
 }
 
 const buttons = document.querySelectorAll("#button1, #button2, #button3");
-buttons.forEach(function(button) {
-    button.addEventListener("click", function() {
-        game(button.textContent.toLowerCase());
-    });
+buttons.forEach(function (button) {
+  button.addEventListener("click", function () {
+    game(button.textContent.toLowerCase());
+  });
 });
